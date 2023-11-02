0d111841960663e28084dc16ec41b5abc293971a
Eli <5178396+gl00ten@users.noreply.github.com>
2023-11-02 07:34:26 +0000

    Update removeduplicatesfromtwolists.py

--- removeduplicatesfromtwolists.py
+++ removeduplicatesfromtwolists.py
@@ -2,26 +2,10 @@
 # note: this is easier to do with sets
 
 
-import copy
-
-def removedups(list1,list2):
-    initiallist1 = copy.deepcopy(list1)
-    commonList = []
-    for element in initiallist1:
-        if element in list2:
-            list1.remove(element)
-            list2.remove(element)
-            commonList.append(element)
-    list1.sort()
-    list2.sort()
-    commonList.sort()
-    return list1,list2,commonList
-
-#test
-list1 = ['b','a','d']
-list2 = ['a','b','c']
-print(removedups(list1,list2))
-
+def removedups(list1, list2):
+    set1, set2 = set(list1), set(list2)
+    commonSet = set1 & set2
+    return sorted(set1 - commonSet), sorted(set2 - commonSet), commonSet
 
 phpfpm = ['make','php5','php-apc','php5-curl','php5-dev','php5-fpm','php-pear','python-simpl*8/ejson',]
 apache = ['php5','php5-dev','make','php-apc', 'php-pear', 'php5-curl', 'php5-mcrypt', 'php5-memcached', 'php5-suhosin',]
